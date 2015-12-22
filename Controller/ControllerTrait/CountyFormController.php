4acc8e5c4fef1f1b443e9adc7f6fdaecfa5a6a20
lighthart <dlighthart@mesd.k12.or.us>
2015-12-22 12:11:35 -0800

    Some index screens adjusted

---
+++ Controller/ControllerTrait/CountyFormController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Lthrt\ContactBundle\Controller\ControllerTrait;
+use Lthrt\ContactBundle\Entity\County;
+use Lthrt\ContactBundle\Form\CountyType;
+
+//
+// CountyFormController Trait.
+//
+
+
+trait CountyFormController
+{
+
+
+    //
+    // Creates a form to create a County entity.
+    //
+    // @param County $county The entity
+    //
+    // @return \Symfony\Component\Form\Form The form
+    //
+    private function createCreateForm(County $county)
+    {
+        $form = $this->createForm(new CountyType(), $county, [
+            'action' => $this->generateUrl('county_create'),
+            'method' => 'POST',
+        ]);
+
+        $form->add('submit', 'submit', [ 'label' => 'Create' ]);
+
+        return $form;
+    }
+
+    //
+    // Creates a form to delete a County entity by id.
+    //
+    // @param mixed $county The entity id
+    //
+    // @return \Symfony\Component\Form\Form The form
+    //
+    private function createDeleteForm(County $county)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('county_delete', [ 'county' => $county->getId() ]))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', [ 'label' => 'Delete' ])
+            ->getForm()
+        ;
+    }
+
+    //
+    // Creates a form to edit a County entity.
+    //
+    // @param County $county The entity
+    //
+    // @return \Symfony\Component\Form\Form The form
+    //
+    private function createEditForm(County $county)
+    {
+        $form = $this->createForm(new CountyType(), $county, [
+            'action' => $this->generateUrl('county_update', [ 'county' => $county->getId() ]),
+            'method' => 'PUT',
+        ]);
+
+        $form->add('submit', 'submit', ['label' => 'Update']);
+
+        return $form;
+    }
+
+}
---
+++ Form/CountyType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Lthrt\ContactBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CountyType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('state')
+            ->add('city')
+            ->add('zip')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Lthrt\ContactBundle\Entity\County'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'lthrt_contactbundle_county';
+    }
+}
