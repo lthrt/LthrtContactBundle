9fe1d692713fea83ee71478830a15f4ba3c8b353
lighthart <dlighthart@mesd.k12.or.us>
2016-01-20 15:33:04 -0800

    Demotype

---
+++ Entity/DemographicType.php
@@ -0,0 +1,27 @@
+<?php
+namespace Lthrt\ContactBundle\Entity;
+use Lthrt\EntityJSONBundle\Entity\UnloggedEntity;
+/**
+ * DemographicType
+ */
+class DemographicType extends UnloggedEntity implements \JSONSerializable
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+
+    /** jsonSerialize
+      *
+      */
+    public function JSONSerialize()
+    {
+        return [
+            'class' => 'Lthrt_ContactBundle_Entity_DemographicType',
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+    }
+
+}
