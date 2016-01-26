683182d5f6a0ed6d9408f5b93e741ab433f85edb
lighthart <dlighthart@mesd.k12.or.us>
2016-01-26 08:11:13 -0800

    Fixing indentation

--- Entity/DemographicType.php
+++ Entity/DemographicType.php
@@ -1,6 +1,8 @@
 <?php
 namespace Lthrt\ContactBundle\Entity;
+
 use Lthrt\EntityJSONBundle\Entity\UnloggedEntity;
+
 /**
  * DemographicType
  */
@@ -11,16 +13,15 @@ class DemographicType extends UnloggedEntity implements \JSONSerializable
      */
     protected $name;
 
-
     /** jsonSerialize
-      *
-      */
+     *
+     */
     public function JSONSerialize()
     {
         return [
             'class' => 'Lthrt_ContactBundle_Entity_DemographicType',
-            'id' => $this->id,
-            'name' => $this->name,
+            'id'    => $this->id,
+            'name'  => $this->name,
         ];
     }
 
