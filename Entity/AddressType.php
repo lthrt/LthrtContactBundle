38bb9d0d0273055fcb87007b6c102aad144c5971
lighthart <lighthart.coder@gmail.com>
2016-02-18 22:13:23 -0800

    Need to finsih converting to annotations/traits

--- Entity/AddressType.php
+++ Entity/AddressType.php
@@ -2,30 +2,36 @@
 
 namespace Lthrt\ContactBundle\Entity;
 
-use Lthrt\EntityJSONBundle\Entity\LoggedEntity;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * AddressType
+ * @ORM\Table()
+ * @ORM\Entity
  */
 
-class AddressType extends LoggedEntity implements \JSONSerializable
+class AddressType implements \JSONSerializable
 {
+    use \Lthrt\EntityJSONBundle\Entity\EntityTrait;
+    use \Lthrt\EntityJSONBundle\Entity\LoggedTrait;
+    use \Lthrt\EntityJSONBundle\Entity\ActiveTrait;
+
     /**
      * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
      */
     protected $name;
 
-
-
     /** jsonSerialize
-      *
-      */
+     *
+     */
     public function JSONSerialize($full = true)
     {
         $json = [
             'class' => 'Lthrt_ContactBundle_Entity_AddressType',
-                'id' => $this->id,
-                'name' => $this->name,
+            'id'    => $this->id,
+            'name'  => $this->name,
         ];
 
         return $json;
