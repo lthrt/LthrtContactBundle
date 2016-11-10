486192303585849e69365d984c47653635ab34c2
lighthart <dlighthart@mesd.k12.or.us>
2016-11-10 10:33:36 -0800

    Token use statements added

--- Command/LoadStatesCommand.php
+++ Command/LoadStatesCommand.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken as Token;
 
 class LoadStatesCommand extends ContainerAwareCommand
 {
--- Command/LoadZipsCommand.php
+++ Command/LoadZipsCommand.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken as Token;
 
 class LoadZipsCommand extends ContainerAwareCommand
 {
